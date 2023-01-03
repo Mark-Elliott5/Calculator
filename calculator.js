596b1e69710933a5348f66651eb8af436c7f9280
Mark Elliott <115184056+Mark-Elliott5@users.noreply.github.com>
2023-01-03 15:32:16 -0500

    Added number event listeners and function

--- calculator.js
+++ calculator.js
@@ -1,3 +1,51 @@
+const previousValue = document.getElementById('previous-value-screen');
+const currentValue = document.getElementById('screen');
+const addButton = document.getElementById('add');
+const subtractButton = document.getElementById('subtract');
+const multiplyButton = document.getElementById('multiply');
+const divideButton = document.getElementById('divide');
+const exponentButton = document.getElementById('exponent');
+const squareRootButton = document.getElementById('squareroot');
+const piButton = document.getElementById('pi');
+const clearButton = document.getElementById('clear');
+const backspaceButton = document.getElementById('backspace');
+const lnButton = document.getElementById('ln');
+const logButton = document.getElementById('log10');
+const nineButton = document.getElementById('nine');
+const eightButton = document.getElementById('eight');
+const sevenButton = document.getElementById('seven');
+const sixButton = document.getElementById('six');
+const fiveButton = document.getElementById('five');
+const fourButton = document.getElementById('four');
+const threeButton = document.getElementById('three');
+const twoButton = document.getElementById('two');
+const oneButton = document.getElementById('one');
+const zeroButton = document.getElementById('zero');
+const decimalButton = document.getElementById('decimal');
+const plusminusButton = document.getElementById('plus-minus');
+const equalsButton = document.getElementById('equals');
+
+previousValue.textContent = '';
+currentValue.textContent = '0';
+
+addNumberListeners()
+
+function addNumberListeners() {
+    let numbers = document.getElementsByClassName('number');
+    for (let i = 0; i < numbers.length; i++) {
+        numbers[i].addEventListener('click', () => {
+            if (currentValue.textContent === '0') {
+                currentValue.textContent = numbers[i].textContent;
+            } else {
+                currentValue.textContent += numbers[i].textContent;
+            }
+        });
+    }
+}
+
+// previousValue.textContent = currentValue.textContent; for operator listeners
+
+
 function addition(existingNumber, input) {
     let result = existingNumber + input;
     console.log(result);

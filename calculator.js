b8c5dd67fcaab7bd12acac8eeec52ae9afa1fe15
Mark Elliott <115184056+Mark-Elliott5@users.noreply.github.com>
2023-01-09 16:57:25 -0500

    Fixed layout, script changes

--- calculator.js
+++ calculator.js
@@ -1,4 +1,4 @@
-const buttons = document.getElementById('buttonsgrid');
+const buttons = document.getElementById('buttons-grid');
 const screen = document.getElementById('screen');
 const previousScreen = document.getElementById('previous-value-screen');
 let currentOperator = '';
@@ -14,33 +14,49 @@ buttons.addEventListener('click', (event) => {
     let operatorJustHit = false;
 
     if ((event.target.dataset.type == 'number')) {
-        if (screen.textContent.length < 9) {
-            if (currentValue === '0') {
-                screen.textContent = event.target.value;
-                return;
+        if (!operatorJustHit) {
+            if (screen.textContent.length < 9) {
+                if (currentValue === '0') {
+                    screen.textContent = event.target.value;
+                    return;
+                } else {
+                    screen.textContent += event.target.value;
+                    return;
+                }
             } else {
-                screen.textContent += event.target.value;
                 return;
             }
-        } else {
-            return;
+        } if (operatorJustHit) {
+            screen.textContent = 0;
+            operatorJustHit = false;    
+            currentValue = operate(previousValue, currentValue, operator);
+            screen.textContent = event.target.value;
         }
-        
+    }
+
+    if (event.target.id === 'clear') {
+        screen.textContent = 0;
+    }
+
+    if (event.target.dataset.type === 'operator') {
+        currentOperator = event.target.value;
+        operatorJustHit = true;
+        previousScreen.textContent = currentValue;
     }
 })
 
-function operate(operator) {
-    if (operator == '+') {
-        let result = previousValue + currentValue;
-        previousValue, currentValue = result;
-    } if (operator == '-') {
-        let result = previousValue - currentValue;
-        previousValue, currentValue = result;
-    } if (operator == '/') {
-        let result = previousValue / currentValue;
-        previousValue, currentValue = result;
-    } if (operator == '*') {
-        let result = previousValue + currentValue;
-        previousValue, currentValue = result;
-    }
-}
+// function operate(previousValue, currentValue, operator) {
+//     if (operator == '+') {
+//         let result = previousValue + currentValue;
+//         return result;
+//     } if (operator == '-') {
+//         let result = previousValue - currentValue;
+//         return result;
+//     } if (operator == '/') {
+//         let result = previousValue / currentValue;
+//         return result;
+//     } if (operator == '*') {
+//         let result = previousValue + currentValue;
+//         return result;
+//     }
+// }
